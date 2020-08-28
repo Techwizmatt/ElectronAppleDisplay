525a350afaa68fb9d48e6043c2082f89d65e3d27
Matthew Maggio <me@techwizmatt.info>
2020-08-28 14:38:01 -0700

    init commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+    $.getJSON( "data.json", function( data ) {
+
+        const img = new Image();
+        const body = {
+            width: 1024,
+            height: 600
+        }
+        const content = $("#content").add("img")
+        content.hide()
+        img.src = data.content.src;
+
+        img.onload = function() {
+            const constraints = {
+                width: this.width * (data.content.size / 100),
+                height: this.height * (data.content.size / 100),
+            }
+            const position = {
+                top: (body.height - constraints.height) / 2,
+                left: (body.width - constraints.width) / 2
+            }
+
+            content.width(constraints.width).height(constraints.height);
+            content.css({top: position.top + "px", left: position.left + "px"});
+            content.fadeIn("slow")
+        }
+
+        content.attr("src",img.src);
+    });
+});
+
+
+
